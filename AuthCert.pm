77f50df876a4f3c22834d515671b048cf526d8b1
Quentin Smith <quentin@mit.edu>
2010-11-15 00:31:25 -0500

    Read ACL from a file instead of checking group membership

--- AuthCert.pm
+++ AuthCert.pm
@@ -6,21 +6,20 @@ use strict;
 # if sucessful or undef otherwise
 sub authenticate {
 	my $r = shift;
-	
-	my $user = $r->subprocess_env('SSL_CLIENT_S_DN_Email');
-	return unless $user =~ /\@MIT.EDU$/i;
-	
-	$user =~ s/\@MIT.EDU$//g;
-	my @groups = split(' ', `groups $user`);
 
-	my $validuser = 0;
-	foreach (@groups) {
-			if (/tv1e|tv1e-users/) {
-					$validuser = 1;
-					last;
-			}
-	}
-	
+	my $user = $r->subprocess_env('SSL_CLIENT_S_DN_Email');
+	$user =~ s/\@MIT\.EDU$//i; # strip @mit.edu from MIT certs
+
+	return unless $user;
+
+	my $aclFile = "/etc/apache2/perl/CookieMonster/cert_acl";
+	open(ACL, "<", $aclFile) or do { warn "Failed to open $aclFile: $!"; return '' }
+	my @users = <ACL>;
+	close(ACL);
+	chomp @users;
+
+	my $validuser = grep { $_ eq $user } @users;
+
 	return ($validuser ? $user : '');
 }
 
